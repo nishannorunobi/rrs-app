fa2ea630c347bd6fbb4bf7065673160869d4fa5f
Nishan <nishancse@gmail.com>
2021-08-06 22:38:20 +0600

    added barchart first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Barchart from './components/barcharts.js'
+
+const App = () => {
+  return (
+      <div>
+        <Barchart/>
+      </div>
+    )
+}
+
+export default App
