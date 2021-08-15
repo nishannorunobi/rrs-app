1df63079cb76d97be75b7c020ffe59a543ab8c84
Nishan <nishancse@gmail.com>
2021-08-15 22:06:35 +0600

    set width of bar div

--- src/Hello.js
+++ src/Hello.js
@@ -4,12 +4,17 @@ import { HorizontalBar } from 'react-chartjs-2';
 //import axios from 'axios'
 
 export default class Button extends Component {
+  state = {};
   constructor(props) {
     super(props);
     this.state = {
-      error: null,
+      rror: null,
       isLoaded: false,
-      items: []
+      items: [],
+      overall_rank : [.35,.50,.20,.52,.90],
+      similarity_score : [.90,.52,.35,.20,.70],
+      years_of_experience : [.05,.10,.95,.62,.81],
+      hovering_color : 'rgba(0,0,0,1)'
     };
   }
 
@@ -44,41 +49,36 @@ export default class Button extends Component {
           backgroundColor: '#EC932F',
           borderColor: 'rgba(255,99,132,1)',
           borderWidth: 1,
-          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-          hoverBorderColor: 'rgba(255,99,132,1)',
-          data: [.35,.50,.20,.52,.90]
+          hoverBackgroundColor: this.state.hovering_color,
+          hoverBorderColor: this.state.hovering_color,
+          data: this.state.overall_rank
         },
         {
           label: 'Similarity Score',
           backgroundColor: 'rgba(255,99,132,0.2)',
           borderColor: 'rgba(255,99,132,1)',
           borderWidth: 1,
-          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-          hoverBorderColor: 'rgba(255,99,132,1)',
-          data: [.93,.20,.50,.35,.75]
+          hoverBackgroundColor: this.state.hovering_color,
+          hoverBorderColor: this.state.hovering_color,
+          data: this.state.similarity_score
         },
         {
           label: 'Years of experience',
           backgroundColor: 'rgba(99,255,132,0.2)',
           borderColor: 'rgba(99,255,132,1)',
           borderWidth: 1,
-          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-          hoverBorderColor: 'rgba(255,99,132,1)',
-          data: [.93,.20,.50,.35,.75]
-        },
-        {
-          label: 'University Ranking',
-          backgroundColor: 'rgba(99,255,132,0.2)',
-          borderColor: 'rgba(99,255,132,1)',
-          borderWidth: 1,
-          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-          hoverBorderColor: 'rgba(255,99,132,1)',
-          data: [.93,.20,.50,.35,.75]
+          hoverBackgroundColor: this.state.hovering_color,
+          hoverBorderColor: this.state.hovering_color,
+          data: this.state.years_of_experience
         }
       ]
     };
     return (
-      <div>
+      <div
+        style={{
+          width: "1000px"
+        }}
+      >
         <h2>Resume Ranking System</h2>
         <HorizontalBar 
         data={dataHorBar} 
